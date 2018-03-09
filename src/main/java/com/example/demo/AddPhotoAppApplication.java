53fa84b79b06e5182adfb568ad4f46a885818782
Kovanusic <Kovanusic@Kovanusic-PC>
2018-03-09 12:18:00 +0100

    test

--- src/main/java/com/example/demo/AddPhotoAppApplication.java
+++ src/main/java/com/example/demo/AddPhotoAppApplication.java
@@ -2,10 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.example.config.JwtFilter;
 
 @SpringBootApplication
 public class AddPhotoAppApplication {
-
+	@Bean
+	public FilterRegistrationBean jwtFilter() {
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/user/*");
+		return registrationBean;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AddPhotoAppApplication.class, args);
 	}
