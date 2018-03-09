ff2185ccebdd0001e3753aa047fc6170ad08386c
Kovanusic <Kovanusic@Kovanusic-PC>
2018-03-09 12:12:37 +0100

    prvi komit

---
+++ src/main/java/com/example/demo/AddPhotoAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddPhotoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddPhotoAppApplication.class, args);
+	}
+}
